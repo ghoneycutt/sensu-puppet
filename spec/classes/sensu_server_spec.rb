07cbac98c21fd239e42f153d4a0b3fb5fe1b10dc
Alessandro Franceschi <al@example42.com>
2017-12-28 18:57:29 +0100

    added tests for darwin and windows for sensu::server::service

--- spec/classes/sensu_server_spec.rb
+++ spec/classes/sensu_server_spec.rb
@@ -36,4 +36,33 @@ describe 'sensu' do
       :hasrestart => false
     ) }
   end # with hasrestart=false
+
+  context 'on Darwin' do
+    let(:facts) do
+      {
+        :osfamily => 'Darwin',
+        :kernel   => 'Darwin',
+        :macosx_productversion_major => '10.12',
+      }
+    end
+    it { should_not contain_service('sensu-server') }
+  end # On Darwin sensu server is not supported
+
+  context 'on Windows' do
+    let(:facts) do
+      {
+        :operatingsystem => 'Windows',
+        :kernel          => 'windows',
+        :osfamily        => 'windows',
+        :os              => {
+          :architecture => 'x64',
+          :release => {
+            :major => '2012 R2',
+          },
+        },
+      }
+    end
+    it { should_not contain_service('sensu-server') }
+  end # On Windows sensu server is not supported
+
 end
